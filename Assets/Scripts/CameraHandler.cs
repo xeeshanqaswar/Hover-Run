4822c48b40a167f3ddccbe1788c52fc317bba816
Zeeshan Qaswar <xeeshanqaswar@gmail.com>
2021-01-31 22:22:04 +0500

    Initial commit

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+
+    public float lerpSpeed;
+    public float rotLerpSpeed = 4f;
+    public Transform camFollow;
+    
+    void FixedUpdate()
+    {
+        if (camFollow != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, camFollow.position, lerpSpeed * Time.deltaTime);
+            Quaternion newRotation = Quaternion.LookRotation(camFollow.forward, transform.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation , newRotation, rotLerpSpeed * Time.deltaTime);
+        }
+    }
+
+}
---
+++ Assets/Scripts/HoverMotorAudio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverMotorAudio : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+    private const float pitchControlFactor = 0.1f;
+    public  float minPitch = 0.1f;
+    public  float maxPitch = 2.0f;
+
+    private Vector3 myVelocity;
+    private Rigidbody carRigidbody;
+
+    private void Awake()
+    {
+        carRigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void FixedUpdate()
+    {
+        myVelocity = carRigidbody.velocity;
+        float forwardSpeed = Mathf.Abs( transform.InverseTransformDirection(myVelocity).z );
+        float controlledPitch = forwardSpeed * pitchControlFactor;
+        audioSource.pitch = Mathf.Clamp(controlledPitch, minPitch, maxPitch);    
+    }
+
+}
